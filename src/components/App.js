08ebee070eef5248010a7b89cebb097ba0d51970
Valeriia <valeria.chyornaya@gmail.com>
2020-02-09 10:43:59 +0200

    Homework react 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Profile from './Profile-card/Profile';
+import Profile from './ProfileCard/Profile';
 import user from '../data/user.json';
 import Statistics from './Statistics/Statistics';
 import statisticalData from  '../data/statistical-data.json';
@@ -11,14 +11,9 @@ import transactions from  '../data/transactions.json';
 export default function App() {
   return (
     <>
-      <h1>Home work 1</h1>
-      <h2>Task 1: Profile-card</h2>
       <Profile user = {user} />
-      <h2>Task 2: Statistics section</h2>
       <Statistics title="Upload stats" stats={statisticalData} />
-      <h2>Task 3: Friend List</h2>
       <FriendList friends={friends} />
-      <h2>Task 4: Transaction History</h2>
       <TransactionHistory items={transactions} />
     </>
   );
--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -4,40 +4,37 @@ import toUpperCaseFirstLetter from '../../utils/toUpperCaseFirstLetter';
 import styles from './TransactionHistory.module.css';
 
 function TransactionHistory({ items }) {
-    return (
-        <table className={styles.transactionHistory}>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
-        <tbody>
-      {items.map((el,inx) => 
-        (<tr key = {el.id} className = {inx%2 ? styles.blue : styles.white}>
+  return (
+    <table className={styles.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((el, inx) => (
+          <tr key={el.id} className={inx % 2 ? styles.blue : styles.white}>
             <td>{toUpperCaseFirstLetter(el.type)}</td>
             <td>{el.amount}</td>
             <td>{el.currency}</td>
           </tr>
-          ))} 
-        </tbody>
-      </table>
-    );
-  }
-  
-//   TransactionHistory.defaultProps = {
-//     items: {},
-//   };
-  
-  TransactionHistory.propTypes = {
-    friends: PropTypes.arrayOf(
-      PropTypes.exact({
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
       id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
       amount: PropTypes.string.isRequired,
       currency: PropTypes.string.isRequired,
-      }))
-  };
-  
-  export default TransactionHistory;
+    }),
+  ),
+};
+
+export default TransactionHistory;
